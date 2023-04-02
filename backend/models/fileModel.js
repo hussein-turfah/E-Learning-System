c4fb1c52e515660c57032a3c92bfbecb71c88623
Hussein Turfah <husseinturfah299@gmail.com>
2023-04-03 01:28:57 +0300

    Created a model for files

---
+++ backend/models/fileModel.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+
+
+const fileSchema = new mongoose.Schema({
+  name:{
+    type: String,
+    required: true,
+    unique: true
+  }
+})
+
+
+const File = mongoose.model("File",fileSchema)
+module.exports = File;
