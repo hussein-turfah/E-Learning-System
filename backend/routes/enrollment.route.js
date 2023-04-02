0bbb4712f332b97694140eb7c1f441bfac7db5e4
Hussein Turfah <husseinturfah299@gmail.com>
2023-04-02 22:55:12 +0300

    Added a new route for listing enrolled users

--- backend/routes/enrollment.route.js
+++ backend/routes/enrollment.route.js
@@ -1,7 +1,8 @@
 const Router = require("express")
-const { enroll } = require("../controllers/enrollment.controller")
+const { enroll,listEnrolledUsers } = require("../controllers/enrollment.controller")
 const router = Router()
 
 router.post("/", enroll)
+router.get("/list",listEnrolledUsers)
 
 module.exports = router
