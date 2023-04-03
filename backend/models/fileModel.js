8616f9c85282214954521050b22c0c644e4c8d25
Hussein Turfah <husseinturfah299@gmail.com>
2023-04-03 07:24:20 +0300

    Added some elements in file schema

--- backend/models/fileModel.js
+++ backend/models/fileModel.js
@@ -5,10 +5,12 @@ const fileSchema = new mongoose.Schema({
   name:{
     type: String,
     required: true,
-    unique: true
+  },file_path: {
+    type: String,
+    required: true,
+    unique: true,
   }
 })
-
 
 const File = mongoose.model("File",fileSchema)
 module.exports = File;
