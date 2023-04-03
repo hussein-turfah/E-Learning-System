62b60cf685ee2a54d373904f7baa7071bb760f67
Hussein Turfah <husseinturfah299@gmail.com>
2023-04-03 07:19:58 +0300

    Created a function for downloading files, and a function for listing files available

--- backend/controllers/file.controller.js
+++ backend/controllers/file.controller.js
@@ -1,6 +1,8 @@
 const formidable = require("formidable");
 const fs = require("fs");
 const File = require("../models/fileModel");
+const directoryPath = './public/files/'
+const http = require('http');
 
 exports.fileUpload = async (req, res) => {
   const form = new formidable.IncomingForm();
@@ -11,7 +13,7 @@ exports.fileUpload = async (req, res) => {
     const old_file_name = files.file.originalFilename;
     const new_file_name = files.file.newFilename;
     const file_ext = old_file_name.split(".").pop();
-    const new_path = "./public/files/" + new_file_name + "." + file_ext;
+    const new_path = directoryPath + new_file_name + "." + file_ext;
 
     fs.rename(old_path, new_path, function (err) {
       if (err) throw err;
@@ -19,8 +21,31 @@ exports.fileUpload = async (req, res) => {
 
     const file = new File()
     file.name = new_file_name
+    file.file_path = new_path; 
     file.save();
   
     res.status(201).json(file)
   });
 };
+exports.fileDownload = async(req,res)=>{
+
+  const {file_name} = req.body
+
+  const file_directory = "http://localhost:" + process.env.PORT + "/public/files/" + file_name
+
+  const file = fs.createWriteStream(`${file_name}`);
+
+  const request = http.get(file_directory, (response) => {
+   response.pipe(file);
+
+   file.on("finish", () => {
+      file.close();
+      res.json("File is being downloaded!")
+   })
+  })
+}
+exports.fileList = async (req, res) => {
+  fs.readdir(directoryPath, (err, files) => {
+    res.json(files);
+  })
+}
